c17f3cc9b934efe4ce8c4cee6750b15d18f8abfc
SulthanRaghib <raghib.smp@gmail.com>
2022-09-28 10:02:17 +0700

    add praktikum 01

---
+++ Praktikum-01/Biadata.java
@@ -0,0 +1,17 @@
+// PRAKTIKUM - 01
+
+// 1. Buatlah program JAVA yang dapat menghasilkan output data diri Anda!
+// 2. Screenshot tampilannya dan upload pada kantung tugas di Elena.
+
+
+public class Biadata {
+   public static void main(String[] args) {
+      System.out.println("===========================");
+      System.out.println("        MY BIODATA");
+      System.out.println("===========================");
+      System.out.println("Nama: Sulthan Raghib Fillah");
+      System.out.println("NIM: 0110221007");
+      System.out.println("Jurusan: Teknik Informatika");
+      System.out.println("Fakultas: Teknik");
+   }
+}
