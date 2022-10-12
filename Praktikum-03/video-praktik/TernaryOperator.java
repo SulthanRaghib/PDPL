41ae77a8888437466bd6aaf67f6f9f49d18afec2
SulthanRaghib <raghib.smp@gmail.com>
2022-10-12 08:03:07 +0700

    upload contoh ternary operator

---
+++ Praktikum-03/video-praktik/TernaryOperator.java
@@ -0,0 +1,6 @@
+public class TernaryOperator {
+   public static void main(String[] args) {
+      int floor = 15;
+      System.out.println("Lantai Sebenarnya: " + (floor > 13 ? floor - 1 : floor));
+   }
+}
