f9f2734a86ab35e6d2dbda5b7d2fd712d73151b9
SulthanRaghib <raghib.smp@gmail.com>
2022-10-12 07:48:00 +0700

    upload contoh selection statement if else

---
+++ Praktikum-03/video-praktik/ifDemo.java
@@ -0,0 +1,12 @@
+// Selection Statement
+public class ifDemo {
+   public static void main(String[] args) {
+      int i = 10;
+
+      if (i < 15) {
+         System.out.println("i is smaller than 15");
+      } else {
+         System.out.println("i is greater than 15");
+      }
+   }
+}
