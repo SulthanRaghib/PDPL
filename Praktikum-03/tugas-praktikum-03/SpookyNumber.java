d7a3b4d302945bc6a92ad80b137bcba1aaa17ebf
SulthanRaghib <raghib.smp@gmail.com>
2022-10-13 23:38:26 +0700

    ngilangin kuning-kuing

--- Praktikum-03/tugas-praktikum-03/SpookyNumber.java
+++ Praktikum-03/tugas-praktikum-03/SpookyNumber.java
@@ -12,22 +12,23 @@ public class SpookyNumber {
    public static void main(String[] args) {
       String ActualFloor = "Selamat datang di Lantai : ";
 
-      Scanner input = new Scanner(System.in);
-      System.out.println("===============================");
-      System.out.println("Nama    : Sulthan Raghib Fillah");
-      System.out.println("NIM     : 0110221007");
-      System.out.println("Roomble : TI01");
-      System.out.println("===============================\n");
+      try (Scanner input = new Scanner(System.in)) {
+         System.out.println("===============================");
+         System.out.println("Nama    : Sulthan Raghib Fillah");
+         System.out.println("NIM     : 0110221007");
+         System.out.println("Roomble : TI01");
+         System.out.println("===============================\n");
 
-      System.out.print("Ingin Naik ke Lantai Berapa : ");
-      int floor = input.nextInt();
+         System.out.print("Ingin Naik ke Lantai Berapa : ");
+         int floor = input.nextInt();
 
-      if (floor == 13) {
-         System.out.println("Tidak ada tombol lantai 13");
-      } else if (floor >= 14) {
-         System.out.println(ActualFloor + (floor - 1));
-      } else {
-         System.out.println(ActualFloor + floor);
+         if (floor == 13) {
+            System.out.println("Tidak ada tombol lantai 13");
+         } else if (floor >= 14) {
+            System.out.println(ActualFloor + (floor - 1));
+         } else {
+            System.out.println(ActualFloor + floor);
+         }
       }
 
       // int floor = 14;
