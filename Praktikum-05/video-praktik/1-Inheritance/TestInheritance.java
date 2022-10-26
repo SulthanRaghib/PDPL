f9771692d99d548fc92eecee15d85ad799cb8af0
Rafii Yuuki 銀 <92235855+SulthanRaghib@users.noreply.github.com>
2022-10-26 11:28:53 +0700

    upload praktik inheritance

---
+++ Praktikum-05/video-praktik/1-Inheritance/TestInheritance.java
@@ -0,0 +1,6 @@
+public class TestInheritance {
+   public static void main(String args[]) {
+      MountainBike tamiya = new MountainBike(100, 200, 10);
+      System.out.println(tamiya.toString());
+   }
+}
