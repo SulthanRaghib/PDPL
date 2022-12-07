47a022aa506b8dad64a98cb86e40e1acf6c645de
Rafii Yuuki 銀 <92235855+SulthanRaghib@users.noreply.github.com>
2022-12-07 09:54:13 +0700

    Langkah 4

---
+++ Praktikum_10/builder/Sony.java
@@ -0,0 +1,16 @@
+// Empat
+
+package Praktikum_10.builder;
+
+public class Sony extends Company {
+   
+   @Override
+   public int price() {
+      return 20;
+   }
+
+   @Override
+   public String pack() {
+      return "Sony CD";
+   }
+}
