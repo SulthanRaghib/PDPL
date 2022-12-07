8d0ad188f4fef8ebfd7bbdea73124756f2bf23c8
Rafii Yuuki 銀 <92235855+SulthanRaghib@users.noreply.github.com>
2022-12-07 09:54:33 +0700

    Langkah 5

---
+++ Praktikum_10/builder/Samsung.java
@@ -0,0 +1,16 @@
+// Lima
+
+package Praktikum_10.builder;
+
+public class Samsung extends Company {
+   
+   @Override
+   public int price() {
+      return 15;
+   }
+
+   @Override
+   public String pack() {
+      return "Samsung CD";
+   }
+}
